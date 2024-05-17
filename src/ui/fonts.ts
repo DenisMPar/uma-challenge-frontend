ed7b5d6b894173baf98218cb01b70d12408b5a97
Denis <denispar.dev@gmail.com>
2024-05-16 21:32:17 -0300

    Project config

---
+++ src/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+});
+export const lato = Lato({ subsets: ["latin"], weight: ["700"] });
