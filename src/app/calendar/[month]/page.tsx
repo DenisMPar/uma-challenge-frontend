6ff45ce9764a3e928bdca28c88c541df7c18fb26
Denis <denispar.dev@gmail.com>
2024-05-19 20:48:44 -0300

    Fixed: added comment

--- src/app/calendar/[month]/page.tsx
+++ src/app/calendar/[month]/page.tsx
@@ -2,6 +2,7 @@ import { generateMonthsPaths } from "@/app/lib/date";
 import { CalendarComponent } from "@/components/calendar";
 import { format, parseISO } from "date-fns";
 export async function generateStaticParams() {
+  //se deberian poner todos los posibes meses pero lo reduje a 8 para evitar tantas llamadas a la api
   const months = generateMonthsPaths(8);
   return months.map((month) => ({
     month: format(parseISO(month), "yyyy-MM"),
