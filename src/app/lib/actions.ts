b67a121f674f9fbe2db494f0c7714f37702e80ef
Denis <denispar.dev@gmail.com>
2024-05-19 14:38:54 -0300

    Feature: added server action

---
+++ src/app/lib/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { revalidatePath, revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function createComment(
+  formData: FormData
+): Promise<{ success: boolean }> {
+  const commentText = await formData.get("commentText");
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/v1/comments/${formData.get("imageDate")}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: commentText }),
+      }
+    );
+    const response = await res.json();
+    revalidateTag("comments");
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
+}
