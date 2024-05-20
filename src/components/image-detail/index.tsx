8022d3fd21e83219890ad4d9ec12c774a0dec125
Denis <denispar.dev@gmail.com>
2024-05-19 21:17:31 -0300

    Feature: added back button

--- src/components/image-detail/index.tsx
+++ src/components/image-detail/index.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 import { CommentsSection } from "./comments";
 import styles from "./styles.module.css";
 import { DetailSearchParams } from "@/app/detail/page";
+import Link from "next/link";
 
 export function ImageDetail({ imageData }: { imageData: DetailSearchParams }) {
   return (
     <div className={styles.imageDetailRoot}>
+      <Link href="/" className={styles.backButton}>
+        Back
+      </Link>
       <div className={styles.imageDetailContainer}>
         <div className={styles.imageTextContainer}>
           <p className={styles.detailText}>{imageData.title}</p>
